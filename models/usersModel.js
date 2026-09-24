36c2b6a70372dead83aa562eb6f2fadd9c7ad714
ezealajoe <www.ezealajoe@gmail.com>
2026-09-24 22:18:27 +0100

    Add user and admin mongoose model

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,8 +1,38 @@
 import mongoose from mongoose
 
 //Added this so you can know where to continue from//
-const userSchema = new mongoose.Schema(
-    {
+const userSchema = new mongoose.Schema({
+    email :{
+        type:String,
+        required:true,
+        unique:tNrue,
+        trim: true,
+        lowercase:true
+    },
+    passwordHash:{
+        type:String,
+        reqiuired:true
+    },
+    fristNmae:{
+        type:String,
+        required:true
+    },
+    MiddelName:{
+        type:String,
+        required: true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],// restricts input to these roles
+        default:'user'//for new signups
 
-    }
-)
+    },
+    timestamps:true
+});
+
+const User = mongoose.model('User',userSchema);
+module.exports=User;
