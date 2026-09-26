dd42fc473f2e24d20d77d44be762644eece03452
Aceda-man <yanjujnronmove@gmail.com>
2026-09-26 07:33:23 +0100

    added model placeholders for team to work with

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,38 +1,35 @@
 import mongoose from "mongoose";
 
-//Added this so you can know where to continue from//
 const userSchema = new mongoose.Schema({
-    email :{
-        type:String,
-        required:true,
-        unique:true,
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
         trim: true,
-        lowercase:true
+        lowercase: true
     },
-    passwordHash:{
-        type:String,
-        required:true
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
     },
-    firstName:{
-        type:String,
-        required:true
-    },
-    middleName:{
-        type:String,
+    passwordHash: {
+        type: String,
         required: true
     },
-    lastName:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:['user','admin'],// restricts input to these roles
-        default:'user'//for new signups
-
-}
-},{
-    timestamps:true
+    role: {
+        type: String,
+        enum: ['organizer', 'member'],
+        default: 'member'
+    }
+}, {
+    timestamps: true
 });
 
 const User = mongoose.model("User", userSchema);
