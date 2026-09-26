86780fd27997062a2ddcf00824e55469067d0949
Abolade90 <aboladeqasim2015@gmail.com>
2026-09-26 13:04:05 +0100

    worked on the GroupModel

---
+++ models/groupModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose"
+
+const groupModelSchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true,
+            trim : true
+        },
+        description : {
+            type : String,
+            trim : true
+        },
+        contributionAmount : {
+            type : Number,
+            required : true,
+            min : 1000
+        },
+        frequency : {
+            type : String,
+            required : true,
+            enum : ["daily", "weekly", "monthly"]
+        },
+        startDate : {
+            type : Date,
+            required : true,
+        },
+        status : {
+            type : String,
+            enum : ["active", "completed"],
+            default : "active"
+        },
+        createdBy : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            requried : true
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+const GroupModel = mongoose.model("GroupModel", groupModelSchema)
+
+export default GroupModel
