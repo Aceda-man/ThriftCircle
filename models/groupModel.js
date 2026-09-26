ffa685c88a8729f365e0675fd8af5e3634bec29f
Abolade90 <aboladeqasim2015@gmail.com>
2026-09-26 14:00:51 +0100

    updated the groupmodel schema

--- models/groupModel.js
+++ models/groupModel.js
@@ -7,33 +7,54 @@ const groupModelSchema = new mongoose.Schema(
             required : true,
             trim : true
         },
-        description : {
-            type : String,
-            trim : true
+        organizer_id : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : User,
+            required : true
         },
-        contributionAmount : {
+        contribution_amount : {
             type : Number,
             required : true,
-            min : 1000
+            min : 1
         },
         frequency : {
             type : String,
             required : true,
             enum : ["daily", "weekly", "monthly"]
         },
-        startDate : {
+        start_date : {
             type : Date,
             required : true,
         },
-        status : {
+        contribution_deadline : {
+            type : Date,
+            required : true       
+        },
+        payout_order : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+            default : []
+        },
+        invite_code : {
+            type : String,
+            required : true,
+            unique : true,
+            trim : true
+        },
+        invite_token : {
+            type : String,
+            required : true,
+            unique : true,
+            trim : true
+        },
+            status : {
             type : String,
             enum : ["active", "completed"],
             default : "active"
         },
-        createdBy : {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "User",
-            requried : true
+        created_at : {
+            type : Date,
+            default : Date.now
         }
     },
     {
